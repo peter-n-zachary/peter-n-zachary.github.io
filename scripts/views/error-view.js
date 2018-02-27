ee1c20dfd0217f60c80754142ab610b4d64387b1
zachary schumpert <buphnezz@gmail.com>
2018-02-27 10:37:10 -0800

    finished error-view.js file

---
+++ scripts/views/error-view.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var app = app || {};
+
+(function (module) {
+  const errorView = {};
+
+  errorView.initErrorPage = function(err) {
+    $('.container').hide();
+    $('.error-view').show();
+    $('#error-message').empty();
+    let template = Handlebars.compile($('#error-template').text());
+    $('#error-message').append(template(err));
+};
+  module.errorView = errorView;
+})(app)
+
+let errorCallback = function(errorObj) {
+  conosole.log(errorObj)
+  errorView.initErrorPage(errorObj); 
+};
