332e1f6c015ad28761c9eb776b9f49817cb2dd1d
Peter Kim <seattlechem@yahoo.com>
2018-03-02 13:06:03 -0800

    update function completed

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -6,7 +6,8 @@
 // page('/', app.Book.fetchAll(app.bookView.initIndexPage));
 page('/', ctx => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/viewDetail/:book_id', ctx => app.Book.fetchOne(ctx, app.bookView.singleBookDisplay));
-page('/books/new', app.bookView.newBookEntryForm);
+page('/books/new', app.bookView.newBook);
 page('/delete/:book_id', ctx => app.Book.fetchOne(ctx, app.bookView.initDeleteBook));
+page('/update/:book_id', ctx => app.Book.fetchOne(ctx, app.bookView.initUpdateBook));
 
 page();
